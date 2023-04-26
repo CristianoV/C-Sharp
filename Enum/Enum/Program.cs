a768294c2cbadb41ee80cd2f3092f61c45205b39
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-25 23:23:05 -0300

    feat: adiciona repositorios de estudo de C#

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,20 @@
+﻿using Enum.Entities;
+using Enum.Entities.Enums;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PerdingPayment
+            };
+
+            Console.WriteLine(order);
+        }
+    }
+}
